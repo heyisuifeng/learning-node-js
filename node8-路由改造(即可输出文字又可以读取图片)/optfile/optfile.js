d1f705b96c47ef3e3fcdf88a7747ed36f7a7de6f
heyisuifeng <1018739994@qq.com>
2016-11-21 20:17:48 +0800

    图片与文本同时输出

---
+++ node8-路由改造(即可输出文字又可以读取图片)/optfile/optfile.js
@@ -0,0 +1,24 @@
+﻿var fs = require('fs');
+
+module.exports={
+	readfile:function(path,recall){
+		fs.readFile(path,function(error,data){
+			if(error){
+				throw error;
+			}
+			recall(data);
+		});
+	},
+	
+	showImages:function(path,response){
+		fs.readFile(path,'binary',function(error,filedata){
+			if(error){
+				console.log(error);
+				return;
+			}else{
+				response.write(filedata,'binary');
+				response.end();
+			}
+		});
+	}
+}
--- node8-路由改造(即可输出文字又可以读取图片)/rout/rout.js
+++ node8-路由改造(即可输出文字又可以读取图片)/rout/rout.js
@@ -1,1 +1,17 @@
-﻿
+﻿var optfile = require('../optfile/optfile.js');//这里返回上级路径
+
+module.exports = {
+	login:function(request,response){
+		response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+		function recall(data){
+			response.write(data);
+			response.end('');
+		}
+		optfile.readfile("./views/html/login.html",recall);//上面已经返回这里不需要在返回
+		//optfile.readfile("../views/html/login.html",recall);错误写法
+	},
+	showImage:function(request,response){
+		response.writeHead(200,{'Content-Type':'image/jpeg'});
+		optfile.showImages("./views/imgs/巴鲁姆克之剑.png",response);//上面已经返回这里不需要在返回
+	}
+}
