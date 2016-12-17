f3478969af931095aa932d213691ac6951958725
heyisuifeng <1018739994@qq.com>
2016-12-17 23:55:58 +0800

    problem

--- node4/node4.js
+++ node4/node4.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var url = require('url');
-var router = require('./rout/rout.js');
+var router = require('./rout/rout');
 http.createServer(function(request,response){
-	response.writeHead(200,{'Content-Type':'text/html,charset=utf-8'});
+	response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 	if(request.url!=="/favicon.ico"){
 		var pathname=url.parse(request.url).pathname;
 		pathname = pathname.replace(/\//,'');//Ìæ»»µôÇ°ÃæµÄ/
