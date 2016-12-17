585dcde4dd0b3f45edf73d8a082c9b834043dd8e
heyisuifeng <1018739994@qq.com>
2016-12-17 17:25:42 +0800

    read file

---
+++ node5/node5.js
@@ -0,0 +1,18 @@
+﻿var http = require('http');
+var optfile = require('./optfile/optfile.js');
+http.createServer(function(request,response){
+	response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+	if(request.url!=="/favicon.ico"){
+		//optfile.readfileSync('./views/login.html');
+		//optfile.readfile('./views/login.html');//如果readfile方法传入了response，异步可能会报错
+		//使用闭包解决
+		function recall(data){
+			response.write(data);
+			console.log('I am waiting');
+			response.end('ok');
+		}
+		optfile.readfile('./views/login.html',recall);
+		console.log("主程序执行完毕");
+	}
+}).listen(8080);
+console.log("Server running 8080..")
---
+++ node5/optfile/optfile.js
@@ -0,0 +1,21 @@
+﻿var fs = require('fs');//fs为node.js自带的操作文件的类
+module.exports={
+	//同步读取
+	readfileSync:function(path){
+		var data=fs.readFileSync(path,'utf-8');
+		console.log(data);
+		console.log("同步方法执行完毕");
+	},
+	//异步读取
+	readfile:function(path,recall){
+		fs.readFile(path,function(error,data){
+			if(error){
+				console.log(error);
+			}else{
+				console.log(data.toString());
+				recall(data);
+			}
+		});
+		console.log("异步方法执行完毕");
+	}
+}
