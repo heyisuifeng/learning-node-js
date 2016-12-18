f45b26d0f990d12c5354fa06749f79c5c59e7679
heyisuifeng <1018739994@qq.com>
2016-12-19 01:12:15 +0800

    文件写入

--- node6/node6.js
+++ node6/node6.js
@@ -1,1 +1,11 @@
-﻿
+﻿var http = require('http');
+
+http.createServer(function(request,response){
+	response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+	if(request.url!=="/favicon.ico"){
+		response.write("文件的写入");
+		console.log("文件写入结束");
+		response.end('');
+	}
+}).listen(8081);
+console.log("Server running at 8081...");
