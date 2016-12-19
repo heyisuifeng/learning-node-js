f57000d0bb6e16c877524cf48b5bb2bc75c77f50
heyisuifeng <1018739994@qq.com>
2016-12-20 00:46:33 +0800

    文件的写入

--- node6/node6.js
+++ node6/node6.js
@@ -1,11 +1,14 @@
 ﻿var http = require('http');
-
+var optfile = require("./optfile/optfile.js");
 http.createServer(function(request,response){
 	response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 	if(request.url!=="/favicon.ico"){
-		response.write("文件的写入");
-		console.log("文件写入结束");
-		response.end('');
+		function recall(data){
+			response.write(data);
+			console.log('I am waiting');
+			response.end('');
+		}
+		optfile.writeFile("./views/writefile.txt","第六章文件的写入",recall);
 	}
 }).listen(8081);
 console.log("Server running at 8081...");
---
+++ node6/optfile/optfile.js
@@ -0,0 +1,38 @@
+﻿var fs = require('fs');//fs为node.js自带的操作文件的类
+module.exports={
+	//同步读取
+	readfileSync:function(path){
+		var data=fs.readFileSync(path,'utf-8');
+		console.log(data);
+		console.log("同步方法执行完毕");
+	},
+	//异步读取
+	readfile:function(path,recall){
+		fs.readFile(path,function(error,data){
+			if(error){
+				console.log(error);
+			}else{
+				console.log("文件读取成功");
+				recall(data);
+			}
+		});
+		console.log("异步方法执行完毕");
+	},
+	
+	//同步写文件
+	writeFileSync:function(path,data){
+		fs.writeFileSync(path,data);
+		console.log("同步写文件完成");
+	},
+	//异步写文件
+	writeFile:function(path,data,recall){
+		fs.writeFile(path,data,function(er){
+			if(er){
+				throw er;
+			}
+			recall("文件写入完成");
+			console.log("文件写入成功");
+		});
+	}
+	
+}
