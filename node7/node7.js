3591e28a5a4164e50ee9411b1509a3bf8192712a
heyisuifeng <1018739994@qq.com>
2016-12-20 15:29:32 +0800

    读取图片

---
+++ node7/node7.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');
+var optImage = require('./optImage/optImage');
+http.createServer(function(request,response){
+	response.writeHead(200,{'Content-Type':'image/jpeg'});
+	if(request.url!=="/favicon.ico"){
+		optImage.readImg('./imgs/巴鲁姆克之剑.png',response);
+	}
+}).listen(8000);
+console.log('Server running at 8000');
---
+++ node7/optImage/optImage.js
@@ -0,0 +1,14 @@
+﻿var fs = require('fs');//fs为node.js自带的操作文件的类
+module.exports={
+	readImg:function(path,response){
+		fs.readFile(path,'binary',function(error,filedata){
+			if(error){
+				console.log(error);
+				return;
+			}else{
+				response.write(filedata,'binary');
+				response.end();
+			}
+		});
+	}
+}
