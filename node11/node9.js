90c66839ad28a7b236c7581972080d7f3f15d27d
heyisuifeng <1018739994@qq.com>
2017-03-06 21:26:04 +0800

    test

---
+++ node11/node9.js
@@ -0,0 +1,23 @@
+﻿var http = require('http');
+var url = require('url');
+var router = require('./rout/rout.js');
+var myException = require('./exception.js');
+http.createServer(function(request,response){
+	if(request.url!=="/favicon.ico"){
+		try{
+			var pathname = url.parse(request.url).pathname;
+			pathname = pathname.replace(/\//,'');
+			if(pathname=="login"){
+				router[pathname](request,response);
+			}else{
+				myException.exceptionFun(0);
+			}
+		}catch(error){
+			console.log("异常："+error);
+			response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+			response.write(error.toString());
+			response.end('');
+		}
+	}
+}).listen(8000);
+console.log("Server running at 8000...");
